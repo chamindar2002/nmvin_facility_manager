e3a1d358d0e31b942b774a81a683821ce564ac3c
chaminda <chamindar2002@yahoo.com>
2016-05-15 00:04:38 +0530

    decouple db configs

---
+++ protected/config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+
+        'connectionString' => 'mysql:host=localhost;dbname=nmwndb_asiast',
+        'emulatePrepare' => true,
+        'username' => 'root',
+        'password' => 'password',
+        'charset' => 'utf8',
+        'enableProfiling'=>true,
+        'enableParamLogging'=>true,
+
+);
---
+++ protected/config/db2.php
@@ -0,0 +1,16 @@
+<?php
+
+
+return array(
+
+        'connectionString' => 'mysql:host=localhost;dbname=nmwndb',
+        'emulatePrepare' => true,
+        'username' => 'root',
+        'password' => 'password',
+        'charset' => 'utf8',
+        'class'   => 'CDbConnection',
+        'enableProfiling'=>true,
+        'enableParamLogging'=>true,
+
+);
+
