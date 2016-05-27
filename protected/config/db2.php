0b31a504bad46f93ed884c499768e4ef04e90a1e
chaminda <chamindar2002@yahoo.com>
2016-05-28 01:13:52 +0530

    projects->block listing update modal  nearly complete. todo -> auto add block records

--- protected/config/db2.php
+++ protected/config/db2.php
@@ -6,7 +6,7 @@ return array(
         'connectionString' => 'mysql:host=localhost;dbname=nmwndb',
         'emulatePrepare' => true,
         'username' => 'root',
-        'password' => 'password',
+        'password' => 'ceylonlinux',
         'charset' => 'utf8',
         'class'   => 'CDbConnection',
         'enableProfiling'=>true,
