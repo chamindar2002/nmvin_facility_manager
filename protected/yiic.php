45880b775f598fe3c7d3af8b0feda19f5d6069a8
Oracle <Oracle@Oracle-PC>
2016-08-18 19:54:41 +0530

    protecte/yiic.php path update

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,8 @@
 <?php
 
 // change the following paths if necessary
-$yiic='/var/www/html/nmvin_facility_manager/framework/yiic.php';
+//$yiic='/var/www/html/nmvin_facility_manager/framework/yiic.php';
+$yiic='/var/www/nmvin_facility_manager/framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
 
 require_once($yiic);
